5b931beb4ab99ca073eb1a85276a3a378c1d9b33
Younes DIOURI <younes.diouri@valeo.com>
2018-02-23 14:49:42 +0100

    add images

--- process.php
+++ process.php
@@ -1,7 +1,7 @@
 <?php
 // Configure your Subject Prefix and Recipient here
 $subjectPrefix = '[Contact Form Construction]';
-$emailTo       = '<admin@devitfamily.com>'; //Replace Your Email Here..
+$emailTo       = '<touhamisalma@gmail.com>'; //Replace Your Email Here..
 $errors = array(); // array to hold validation errors
 $data   = array(); // array to pass back data
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
