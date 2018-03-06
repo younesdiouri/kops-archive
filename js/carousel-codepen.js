72f969fc3a558e379d8a5d00ccec5fd03ee6e6d7
Younes DIOURI <younes.diouri@valeo.com>
2018-03-06 12:16:03 +0100

    testimonials changed

---
+++ js/carousel-codepen.js
@@ -0,0 +1,66 @@
+// Get carousel elements
+
+var tLeftButton = $("#testimonials-l");
+var tRightButton = $("#testimonials-r");
+
+// Get number of <li> elements in carousel
+
+var tItemCount = document.getElementById('testimonials-ul').querySelectorAll('li').length;
+
+// Set length based on that
+
+var tWidth = tItemCount * 100 + "vw";
+$(".testimonials ul").css("width", tWidth);
+
+// Button functionality
+
+var tPosition = 0;
+console.log(tPosition);
+
+tRightButton.click(function() {
+    if (tPosition < (tItemCount - 1)) {
+        tPosition++;
+        var m = "-" + (100 * tPosition) + "vw";
+        $(".testimonials ul").animate({
+            "left": m
+        }, 500);
+        greyButton();
+    }
+});
+
+tLeftButton.click(function() {
+    if (tPosition > 0) {
+        tPosition--;
+        var m = "-" + (100 * tPosition) + "vw";
+        $(".testimonials ul").animate({
+            "left": m
+        }, 500);
+        greyButton();
+    }
+});
+
+// Grey out buttons if not useable
+
+var greyButton = function() {
+    if (tPosition == 0) {
+        tLeftButton.css("opacity", "0.3");
+        tLeftButton.css("cursor", "default");
+    } else if (tPosition == (tItemCount - 1)) {
+        tRightButton.css("opacity", "0.3");
+        tRightButton.css("cursor", "default");
+    } else {
+        tRightButton.css("opacity", "1");
+        tRightButton.css("cursor", "pointer");
+        tLeftButton.css("opacity", "1");
+        tLeftButton.css("cursor", "pointer");
+    }
+}
+
+greyButton();
+
+// And finally, if there's only one quote, kill the buttons altogether
+
+if ( tItemCount == 1 ) {
+    $('.testimonials-control').css('display','none');
+}
+
